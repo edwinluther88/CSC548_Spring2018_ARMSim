7718c3dad331d4f9462b9b454dcf701837ab6060
edwinluther88 <33527311+edwinluther88@users.noreply.github.com>
2018-03-08 16:54:45 -0600

    Homework changes

--- app/src/main/java/com/example/awesomefat/csc548_spring2018_armsim/MainActivity.java
+++ app/src/main/java/com/example/awesomefat/csc548_spring2018_armsim/MainActivity.java
@@ -4,14 +4,21 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText inputET;
+    String[] strArray;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        inputET = (EditText)findViewById(R.id.inputET);
     }
 
     public void onRegistersButtonClicked(View v)
@@ -30,7 +37,52 @@ public class MainActivity extends AppCompatActivity {
     {
         //Support 2 instructions: ADD and ADDI
         //ADD assumes 2 registers
-        //ADDI assumes 1 register and one numeric literal
-        System.out.println(CORE.X0);
+        //ADDI assumes 1 registers and one numeric literal
+
+        if (this.inputET.getText().toString().equals(""))
+        {
+            Toast.makeText(MainActivity.this, "Enter instruction",
+                    Toast.LENGTH_SHORT).show();
+            return;
+        }
+        String strValue = inputET.getText().toString();
+
+        strArray = strValue.split(" ");
+
+        if(("ADD").equals(strArray[0]) || ("ADDI").equals(strArray[0]))
+        {
+            int valueOfOperand= getOperandValue(strArray[1]);
+            CORE.registers[valueOfOperand] = sumOfRegisters();
+        }
+        else
+        {
+            Toast.makeText(MainActivity.this, "Invalid instruction code",
+                        Toast.LENGTH_SHORT).show();
+            return;
+        }
+    }
+
+    private String sumOfRegisters()
+    {
+        Integer result;
+        if(strArray[3].charAt(0) == '#'){
+            result = Integer.parseInt(CORE.registers[getOperandValue(strArray[2])]) + getOperandValue(strArray[3]);
+        }
+        else
+        {
+            result = Integer.parseInt(CORE.registers[getOperandValue(strArray[2])]) + Integer.parseInt(CORE.registers[getOperandValue(strArray[3])]);
+        }
+        return result.toString();
+    }
+
+    private int getOperandValue(String tempStr){
+        if(tempStr.charAt(0) == '#')
+        {
+            return Integer.parseInt(tempStr.substring(1));
+        }
+        else
+        {
+            return Integer.parseInt("" + tempStr.charAt(1));
+        }
     }
 }
--- app/src/main/java/com/example/awesomefat/csc548_spring2018_armsim/RegisterActivity.java
+++ app/src/main/java/com/example/awesomefat/csc548_spring2018_armsim/RegisterActivity.java
@@ -2,8 +2,12 @@ package com.example.awesomefat.csc548_spring2018_armsim;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    EditText XET[] = new EditText[CORE.numberOfRegisters];
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -11,6 +15,27 @@ public class RegisterActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
-        CORE.X0 = "2";
+        XET[0] = findViewById(R.id.X0ET);
+        XET[1] = findViewById(R.id.X1ET);
+        XET[2] = findViewById(R.id.X2ET);
+        XET[3] = findViewById(R.id.X3ET);
+        XET[4] = findViewById(R.id.X4ET);
+        XET[5] = findViewById(R.id.X5ET);
+        XET[6] = findViewById(R.id.X6ET);
+        XET[7] = findViewById(R.id.X7ET);
+
+        for(int i=0; i<CORE.numberOfRegisters;i++)
+        {
+            XET[i].setText(CORE.registers[i]);
+        }
+
+    }
+
+    public void onSaveButtonClicked(View v)
+    {
+        for(int i=0; i<CORE.numberOfRegisters; i++)
+        {
+            CORE.registers[i] = XET[i].getText().toString();
+        }
     }
 }
